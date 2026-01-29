2f645c6af64d234532a330a345582789f24a0de2
widadarsi96-create <widadarsi96@gmail.com>
2026-01-29 14:40:00 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,29 @@
-function hasil() {
+function kirimData() {
   const data = {
     email: document.getElementById("email").value,
     password: document.getElementById("password").value,
     nama: document.getElementById("nama").value,
     umur: document.getElementById("umur").value,
     gender: document.querySelector('input[name="gender"]:checked')?.value || "",
-    ttl: document.getElementById("ttl").value,
-    tangalahir: document.getElementById("tangalahir").value,
-    school: document.getElementById("school").value,
+    tempat: document.getElementById("ttl").value,
+    tanggal_lahir: document.getElementById("tanggalahir").value,
+    sekolah: document.getElementById("school").value,
     universitas: document.getElementById("universitas").value,
-    alasan: document.getElementById("alasan").value,
+    alasan: document.getElementById("alasan").value
   };
 
-  fetch("URL_APPS_SCRIPT_LO", {
+  fetch("https://script.google.com/macros/s/AKfycby7jhB_WUN06hDfTKt9XqgatJWWC9mtkSFRXFoiH24MuZ-u2AnTFs9779FvMdyZSquo/exec", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data)
   })
   .then(res => res.text())
-  .then(res => {
-    alert("Data berhasil dikirim");
-    console.log(res);
+  .then(() => {
+    alert("Data berhasil dikirim ke Sheets!");
+    document.getElementById("registForm").reset();
   })
   .catch(err => {
-    alert("Gagal kirim data");
+    alert("Gagal kirim data. Cek console");
     console.error(err);
   });
 }
-
-function hasil() {
-  fetch("https://script.google.com/macros/s/AKfycbzDgwImI_HaLL6sMATcrvM_4hiTv7Ie3ABtU77MrUs0vgXfyJPYKCdK6ut8B5Qu3LQH/exec", { 
-    method: "POST",
-    body: JSON.stringify({
-      nama: document.getElementById("nama").value
-    })
-  });
-}
-
